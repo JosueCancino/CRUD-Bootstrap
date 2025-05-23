ad2e0b18a2e9162ee75cd39c4de4f29617b9823f
JosueCancino <josue.yahel.cancino@gmail.com>
2025-05-23 13:30:37 -0600

    Se agrega el mensaje en el main

--- config/config.php
+++ config/config.php
@@ -1,11 +1,13 @@
 <?php
-$host = "localhost";
-$usuario = "root";
-$contrasena = "";
-$base_de_datos = "bd_empleados";
+$host = "dpg-d0oc1u8dl3ps73du8ekg-a";   // el host de Render
+$dbname = "bd_empleados_5765";          // el nombre de tu base de datos
+$user = "josuecancino";                 // tu usuario
+$password = "UcfOse1UhwBBoIWFyyKgBpURpJhiD1GD";          // tu contraseña
 
-$conexion = new mysqli($host, $usuario, $contrasena, $base_de_datos);
-
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+try {
+    $pdo = new PDO("pgsql:host=$host;port=5432;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error al conectar a la base de datos: " . $e->getMessage());
 }
+?>
